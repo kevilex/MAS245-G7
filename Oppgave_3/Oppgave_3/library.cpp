cfc9e283a78e62ba9462dffb656661e9a877f828
Kevin Hermann Skaar <kevinhsk@uia.no>
2021-10-20 12:00:20 +0200

    Oppgave 3 code

---
+++ Oppgave_3/Oppgave_3/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * Oppgave_3.cpp
+ *
+ * Created: 20.10.2021 11:54:31
+ * Author : kevin
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
