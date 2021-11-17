3605909954e8da497bc8cc7ed32e6cb6ce0faf33
kevilex <Kevin@skaar-net.no>
2021-11-17 19:47:54 +0100

    Update SoftBlink_Simple.cpp

--- SoftBlink/SoftBlink_Simple.cpp
+++ SoftBlink/SoftBlink_Simple.cpp
@@ -1,48 +1,50 @@
 /*
- * SoftBlink_Simple.cpp
+ * GccApplication1.cpp
  *
- * Created: 09.11.2021 13:59:26
+ * Created: 17.11.2021 19:28:54
  * Author : kevin
  */ 
+#define F_CPU 1000000UL
+#define LED_Pin (6)
 
 #include <avr/io.h>
-#define F_CPU 1000000UL //setter clockspeed
-#define LED_Pin (6) //setter en variabel for led-pin
-
-#include<math.h>
-#include<util/delay.h>
-
-
-double dutycycle = 0;
-
-
-
+#include <util/delay.h>
+unsigned int i = 0;
+unsigned int dutycycle = 0;
 
 int main(void)
-{
-	DDRD = (1<<LED_Pin); //PIN 6 på register D er satt til output
-	// Table 15-3 side 101. COM0A1 (Compare Output Mode) gjør at vi får fast pwm som tømmer OC0A når den matcher telleren og setter OC0A på bunnen
-	TCCR0A = (COM0A1<<1)
-	//  Table 15-8 side 103. WGM01 og WGM00 (Waveform Generation Mode) gjør at vi får fast pwm modus og toppen blir 0xFF og man oppdaterer OCRx på bunnen.
-	TCCR0A = (WGM01<<1) | (WGM00<<1);
-	// Table 15-9 side 105. Setter pre-scalar, men siden vi ikke har skalert noe, så setter vi CS00 som 1.
-	TCCR0A = (CS00<<1);
-   
+{	
+	DDRD = (1<<LED_Pin);
+	TCCR0A = (1<<COM0A1) | (1<<COM0B1) | (1<<WGM01) | (1<<WGM00);      // Non inverting mode on OC0A and OC0B, Mode = Mode 3 FAST PWM
+	TCCR0B = (1<<CS00);
+	
+	
     while (1) 
     {
-		//dimmer opp med delay på 10ms slik at man kan se forandringene i lyset.
-		for (dutycycle = 0, dutycycle == 255, dutycycle++)
+		
+	if (i == 0)
+	{
+		_delay_ms(10);
+		dutycycle+=1;
+		OCR0A = dutycycle;
+		if(dutycycle>255)
 		{
-			OCR2A = dutycycle;
-			_delay_ms(10);
+			i = 1;
 		}
-		//dimmer ned med delay på 10ms slik at man kan se forandringene i lyset.
-		for (dutycycle = 255, dutycycle == 0, dutycycle--)
+	}
+	
+	if (i == 1)
+	{
+		_delay_ms(10);
+		dutycycle-=1;
+		OCR0A = dutycycle;
+		if(dutycycle==0)
 		{
-			OCR2A = dutycycle;
-			_delay_ms(10);
+			i = 0;
 		}
+	}
 		
     }
 }
+
 
